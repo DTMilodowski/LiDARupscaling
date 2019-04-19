c3a7661612655d157919281cd74dad5593749040
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-04-19 15:14:26 +0100

    calibration framework

--- src/pt1_lidar_sentinel_fusion_train_rf.py
+++ src/pt1_lidar_sentinel_fusion_train_rf.py
@@ -42,6 +42,13 @@ sys.path.append('./data_visualisation/')
 import data_io as io
 
 """
+Project Info
+"""
+site_id = 'kiuic'
+version = '001'
+path2alg = '../saved_models/'
+
+"""
 #===============================================================================
 PART A: LOAD IN DATA AND SUBSET THE TRAINING DATA
 Load data
@@ -49,7 +56,13 @@ Filter areas where we have LiDAR estimates
 Subsample if desired/required
 #-------------------------------------------------------------------------------
 """
-predictors,AGB,landmask,labels=io.load_predictors()
+# Load predictors & target
+predictors,target,landmask,labels=io.load_predictors()
+
+# Keep only areas for which we have biomass estimates
+mask = np.isfinite(target)
+X = predictors[mask,:]
+y = target[mask]
 
 """
 #===============================================================================
@@ -59,6 +72,44 @@ Cal-val figures
 Importances via permutation importance
 #-------------------------------------------------------------------------------
 """
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,
+                                                    test_size=0.25)
+rf = RandomForestRegressor(bootstrap=True,
+            criterion='mse',           # criteria used to choose split point at each node
+            max_depth=None,            # ***maximum number of branching levels within each tree
+            max_features='auto',       # ***the maximum number of variables used in a given tree
+            max_leaf_nodes=None,       # the maximum number of leaf nodes per tree
+            min_impurity_decrease=0.0, # the miminum drop in the impurity of the clusters to justify splitting further
+            min_impurity_split=None,   # threshold impurity within an internal node before it will be split
+            min_samples_leaf=5,       # ***The minimum number of samples required to be at a leaf node
+            min_samples_split=20,       # ***The minimum number of samples required to split an internal node
+            min_weight_fraction_leaf=0.0,
+            n_estimators=100,          # ***Number of trees in the random forest
+            n_jobs=-1,                 # The number of jobs to run in parallel for both fit and predict
+            oob_score=True,            # use out-of-bag samples to estimate the R^2 on unseen data
+            random_state=None,         # seed used by the random number generator
+            verbose=0)
+
+# fit the calibration sample
+rf.fit(X_train,y_train)
+y_train_rf = rf.predict(X_train)
+cal_score = rf.score(X_train,y_train) # calculate coefficeint of determination R^2 of the calibration
+print("Calibration R^2 = %.02f" % cal_score)
+
+# fit the validation sample
+y_test_rf = rf.predict(X_test)
+val_score = rf.score(X_test,y_test)
+print("Validation R^2 = %.02f" % val_score)
+
+# Plot cal-val
+fig1,axes = gplt.plot_cal_val_agb(y_train,y_train_rf,y_test,y_test_rf)
+
+# Importances
+perm = PermutationImportance(rf).fit(X_test, y_test)
+imp_df = pd.DataFrame(data = {'variable': labels,
+                              'permutation_importance': perm.feature_importances_,
+                              'gini_importance': rf.feature_importances_})
+gplt.plot_importances(imp_df)
 
 """
 #===============================================================================
@@ -67,3 +118,9 @@ Fit model with full training set
 Save model
 #-------------------------------------------------------------------------------
 """
+rf.fit(X,y)
+cal_score = rf.score(X,y) # calculate coefficeint of determination R^2 of the calibration
+print("Calibration R^2 = %.02f" % cal_score)
+
+# Save random forest model for future use
+joblib.dump(rf,'%s%s_%s_rf_sentinel_lidar_agb.pkl' % (path2alg,site_id,version))
