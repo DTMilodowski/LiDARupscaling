ba01c27f8ab6a805d06c8e9416ca5375bb0d5fae
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-05-07 10:25:07 +0100

    plotting UTM

--- src/data_visualisation/map_plots.py
+++ src/data_visualisation/map_plots.py
@@ -11,6 +11,8 @@ import libraries needed
 import numpy as np
 import matplotlib.pyplot as plt     # plotting package
 import seaborn as sns               # another useful plotting package
+import cartopy.crs as ccrs
+
 sns.set()
 
 """
@@ -20,12 +22,14 @@ a very simple script to produce a nice simple map from an xarray object
 """
 def plot_xarray(xarr, figure_name = None,figsize_x=8,figsize_y=6,vmin=None,
                 vmax=None,cmap='viridis',add_colorbar=False,cbar_kwargs={},
-                show=True,title=""):
+                show=True,title="",subplot_kw={}):
     if vmin is None:
         vmin = np.nanmin(xarr)
     if vmax is None:
         vmax =np.nanmax(xarr)
-    fig, axis = plt.subplots(nrows=1, ncols=1, figsize=(figsize_x,figsize_y))
+
+    fig, axis = plt.subplots(nrows=1, ncols=1, figsize=(figsize_x,figsize_y),
+                   subplot_kw=subplot_kw)
     if add_colorbar:
         extend = 'neither'
         if vmin > np.nanmin(xarr.values):
@@ -36,10 +40,10 @@ def plot_xarray(xarr, figure_name = None,figsize_x=8,figsize_y=6,vmin=None,
         else:
             if vmax < np.nanmax(xarr.values):
                 extend = 'max'
-        xarr.plot(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar,
-                    extend=extend, cbar_kwargs=cbar_kwargs)
+        xarr.plot.imshow(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar,
+                    extend=extend, cbar_kwargs=cbar_kwargs,transform = subplot_kw['projection'])
     else:
-        xarr.plot(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar)
+        xarr.plot.imshow(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar,transform = subplot_kw['projection'])
     axis.set_aspect("equal")
     axis.set_title(title,fontsize=16)
     if figure_name is not None:
