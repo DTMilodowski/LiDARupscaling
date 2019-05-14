8c573c13fab85d438166bbd95c1121bf9bd3197f
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-05-14 15:46:23 +0100

    now using bayesian optimisation

---
+++ src/pt1_lidar_sentinel_fusion_train_rf_bayesian.py
@@ -0,0 +1,172 @@
+"""
+pt1_lidar_sentinel_fusion_train_rf_random_bayesian.py
+--------------------------------------------------------------------------------
+FITTING RANDOM FOREST MODEL TO LINK SENTINEL LAYERS TO LIDAR ESTIMATED AGB
+This script loads the predictor (sentinel bands and derivatives) and target
+(lidar estimated AGB) variables, calibrates and validates a random forest
+regression model, and fits a final model using te full training set.
+
+The random forest algroithm is optimised using a two-step process: 1st a
+randomized search is undertaken to locate a reasonable starting point; then a
+bayesian optimiser (TPE) is used to refine the parameterisation.
+
+This code built using the open source programming language python, and utilises
+the geospatial library xarray (http://xarray.pydata.org/en/stable/) and the
+machine learning library scikit-learn
+(https://scikit-learn.org/stable/index.html).
+
+24/04/2019 - D. T. Milodowski
+--------------------------------------------------------------------------------
+"""
+
+"""
+# Import the necessary packages
+"""
+import numpy as np                  # standard package for scientific computing
+import xarray as xr                 # xarray geospatial package
+import pandas as pd                 # data frames
+import matplotlib.pyplot as plt     # plotting package
+import seaborn as sns               # another useful plotting package
+import os
+
+# Import some parts of the scikit-learn library
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.externals import joblib
+
+import eli5
+from eli5.sklearn import PermutationImportance
+
+from hyperopt import tpe, Trials, fmin, hp, STATUS_OK
+
+# Import custom libaries
+
+import sys
+sys.path.append('./random_forest/')
+sys.path.append('./data_io/')
+sys.path.append('./data_visualisation/')
+
+import data_io as io
+import general_plots as gplt
+
+"""
+Project Info
+"""
+site_id = 'kiuic'
+version = '003'
+path2alg = '../saved_models/'
+if(os.path.isdir(path2alg)==False):
+    os.mkdir(path2alg)
+path2fig= '../figures/'
+if(os.path.isdir(path2fig)==False):
+    os.mkdir(path2fig)
+
+"""
+#===============================================================================
+PART A: LOAD IN DATA AND SUBSET THE TRAINING DATA
+Load data
+Filter areas where we have LiDAR estimates
+Subsample if desired/required
+#-------------------------------------------------------------------------------
+"""
+print('Loading data')
+# Load predictors & target
+predictors,target,landmask,labels=io.load_predictors()
+n_predictors = predictors.shape[1]
+
+# Custom mask
+target[0:800,2600:2728] = np.nan
+target[4000:,:2000] = np.nan
+# Keep only areas for which we have biomass estimates
+mask = np.isfinite(target[landmask])
+X = predictors[mask,:]
+y = target[landmask][mask]
+
+"""
+#===============================================================================
+PART B: CAL-VAL USING RANDOMISED SEARCH RF HYPERPARAMETER OPTIMISATION
+Cal-val
+Cal-val figures
+#-------------------------------------------------------------------------------
+"""
+print('Calibration/validation')
+#split train and test subset, specifying random seed for reproducability
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.75,
+                                                test_size=0.25, random_state=29)
+
+
+#define the parameters for the gridsearch
+max_depth_range = range(1,100)
+max_features_range = range(1,n_predictors+1)
+min_samples_leaf_range = range(1,100)
+min_samples_split_range = range(2,500)
+n_estimators_range = range(50,1200)
+
+param_space = { "max_depth":hp.choice("max_depth", max_depth_range),              # ***maximum number of branching levels within each tree
+                "max_features":hp.choice("max_features",max_features_range),      # ***the maximum number of variables used in a given tree
+                "min_samples_leaf":hp.choice("min_samples_leaf",min_samples_leaf_range),    # ***The minimum number of samples required to be at a leaf node
+                "min_samples_split":hp.choice("min_samples_split",min_samples_split_range),  # ***The minimum number of samples required to split an internal node
+                "n_estimators":hp.choice("n_estimators",n_estimators_range),           # ***Number of trees in the random forest
+                "criterion":hp.choice('criterion', ["mse"]),                      # criterion used for to construct forest
+                "bootstrap":hp.choice('bootstrap', [True]),
+                "n_jobs":hp.choice("n_jobs",[20]),
+                "random_state": hp.choice("random_state",29)
+                }
+
+# define a function to quantify the objective function
+best = 0
+def f(params):
+    global best
+    rf = RandomForestRegressor(**params)
+    acc = cross_val_score(clf, X_train, y_train, cv=3).mean()
+    if acc > best:
+        best = acc
+        print('new best: ', best, params)
+    return {'loss': -acc, 'status': STATUS_OK}
+
+trials=Trials()
+best = fmin(f, param_space, algo=tpe.suggest, max_evals=500, trials=trials)
+print('best:')
+print(best)
+
+max_depth_best = np.array(max_depth_range)[best["max_depth"]]
+max_features_best = np.array(max_features_range)[best["max_depth"]]
+min_samples_leaf_best = np.array(min_samples_leaf_range)[best["min_samples_leaf"]]
+min_samples_split_best = np.array(min_samples_split_range)[best["min_samples_split"]]
+n_estimators_best = np.array(n_estimators_range)[best["n_estimators"]]
+
+rf = RandomForestRegressor(bootstrap=True,
+            criterion='mse',           # criteria used to choose split point at each node
+            max_depth= max_depth_best,            # ***maximum number of branching levels within each tree
+            max_features=max_features_best,       # ***the maximum number of variables used in a given tree
+            max_leaf_nodes=None,       # the maximum number of leaf nodes per tree
+            min_impurity_decrease=0.0, # the miminum drop in the impurity of the clusters to justify splitting further
+            min_impurity_split=None,   # threshold impurity within an internal node before it will be split
+            min_samples_leaf=min_samples_leaf_best,       # ***The minimum number of samples required to be at a leaf node
+            min_samples_split=min_samples_split_best,       # ***The minimum number of samples required to split an internal node
+            n_estimators=n_estimators_best,          # ***Number of trees in the random forest
+            n_jobs=-1,                 # The number of jobs to run in parallel for both fit and predict
+            oob_score=True,            # use out-of-bag samples to estimate the R^2 on unseen data
+            random_state=29,         # seed used by the random number generator
+            )
+
+
+# Save random forest model for future use
+joblib.dump(rf_random,'%s%s_%s_rf_sentinel_lidar_agb_bayes_opt.pkl' % (path2alg,site_id,version))
+
+# fit the calibration sample
+rf.fit(X_train,y_train)
+y_train_rf = rf.predict(X_train)
+cal_score = rf.score(X_train,y_train) # calculate coefficeint of determination R^2 of the calibration
+print("Calibration R^2 = %.02f" % cal_score)
+
+# fit the validation sample
+y_test_rf = rf.predict(X_test)
+val_score = rf.score(X_test,y_test)
+print("Validation R^2 = %.02f" % val_score)
+
+# Plot cal-val
+fig1,axes = gplt.plot_cal_val_agb(y_train,y_train_rf,y_test,y_test_rf)
+fig1.savefig('%s%s_%s_cal_val.png' % (path2fig,site_id,version))
