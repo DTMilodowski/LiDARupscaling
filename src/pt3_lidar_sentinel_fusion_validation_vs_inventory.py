2233108cec8d4fb9abed2fd8efdf886fe0f0d1f8
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-08-22 15:05:37 +0100

    plot looking good now

--- src/pt3_lidar_sentinel_fusion_validation_vs_inventory.py
+++ src/pt3_lidar_sentinel_fusion_validation_vs_inventory.py
@@ -29,23 +29,14 @@ import xarray as xr                 # xarray geospatial package
 import matplotlib.pyplot as plt     # plotting package
 import seaborn as sns               # another useful plotting package
 import os
+import pandas as pd
 
-# Import some parts of the scikit-learn library
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.externals import joblib
-
-# Import cartographic projection library
-import cartopy.crs as ccrs
+from sklearn.metrics import r2_score, mean_squared_error
 
 # Import custom libaries
 import sys
 sys.path.append('./data_io/')
-sys.path.append('./data_visualisation/')
-sys.path.append('./random_forest/')
-
 import data_io as io
-import map_plots as mplt
-import random_forest_functions as rff
 
 """
 Project Info
@@ -92,7 +83,7 @@ other_agb_upscaled = []
 
 for ii,plot in enumerate(inventory):
     nearest_x = np.argsort((lidar.coords['x'].values-plot['x'])**2)[0]
-    nearest_y = np.argsort((lidar.coords['y'].values-plot['y']**2))[0]
+    nearest_y = np.argsort((lidar.coords['y'].values-plot['y'])**2)[0]
     lidar_agb = lidar.values[nearest_y,nearest_x]
     if np.isfinite(lidar_agb):
         lidar_agb_field.append(plot['AGB'])
@@ -107,3 +98,46 @@ for ii,plot in enumerate(inventory):
 PART B: COMPARE INVENTORY vs LiDAR/UPSCALED AGB ESTIMATES
 #-------------------------------------------------------------------------------
 """
+lidar_df = pd.DataFrame({'lidar':lidar_agb_lidar,'upscaled':lidar_agb_upscaled,
+                    'plot':lidar_agb_field})
+other_df = pd.DataFrame({'upscaled':other_agb_upscaled,'plot':other_agb_field})
+
+# Now plot up summaries according to the subset in question
+fig,axes = plt.subplots(nrows=1,ncols=3,figsize=[10,3.4],sharex='all',sharey='all')
+
+sns.regplot('lidar','plot',data=lidar_df,ax=axes[0],scatter_kws={'s':2})
+sns.regplot('upscaled','plot',data=lidar_df,ax=axes[1],scatter_kws={'s':2})
+sns.regplot('upscaled','plot',data=other_df,ax=axes[2],scatter_kws={'s':2})
+
+x_labs = ['AGB$_{lidar}$ / Mg ha$^{-1}$',
+        'AGB$_{upscaled}$ / Mg ha$^{-1}$',
+        'AGB$_{upscaled}$ / Mg ha$^{-1}$']
+annotations = ['lidar vs.\ninventory','upscaled vs.\ninventory (lidar)',
+                'upscaled vs.\ninventory (outside)']
+
+from scipy import stats
+
+temp1,temp2,r_a,temp3,temp4 = stats.linregress(lidar_df.dropna()['lidar'],lidar_df.dropna()['plot'])
+temp1,temp2,r_b,temp3,temp4 = stats.linregress(lidar_df.dropna()['upscaled'],lidar_df.dropna()['plot'])
+temp1,temp2,r_c,temp3,temp4 = stats.linregress(other_df.dropna()['upscaled'],other_df.dropna()['plot'])
+
+R2 = [r_a**2,r_b**2,r_c**2]
+
+RMSE = [np.sqrt(mean_squared_error(lidar_df.dropna()['lidar'],lidar_df.dropna()['plot'])),
+    np.sqrt(mean_squared_error(lidar_df.dropna()['upscaled'],lidar_df.dropna()['plot'])),
+    np.sqrt(mean_squared_error(other_df.dropna()['upscaled'],other_df.dropna()['plot']))]
+
+for ii,ax in enumerate(axes):
+    ax.annotate(annotations[ii], xy=(0.05,0.95), xycoords='axes fraction',
+                        backgroundcolor='none',horizontalalignment='left',
+                        verticalalignment='top', fontsize=10)
+    ax.annotate('R$^2$=%.3f\nRMSE=%.1f' % (R2[ii],RMSE[ii]), xy=(0.95,0.05), xycoords='axes fraction',
+                        backgroundcolor='none',horizontalalignment='right',
+                        verticalalignment='bottom', fontsize=10)
+    ax.set_xlabel(x_labs[ii],fontsize=10)
+    ax.set_ylabel('AGB$_{inventory}$ / Mg ha$^{-1}$',fontsize=10)
+    ax.set_aspect('equal')
+
+fig.tight_layout()
+fig.savefig('%s%s_%s_inventory_comparison.png' % (path2output,site_id,version))
+fig.show()
