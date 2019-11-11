98f2a94b19eb4046bd193312027969f4452d4191
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-11-11 14:39:16 +0000

    adding plot to existing axis

--- src/data_visualisation/map_plots.py
+++ src/data_visualisation/map_plots.py
@@ -51,3 +51,31 @@ def plot_xarray(xarr, figure_name = None,figsize_x=8,figsize_y=6,vmin=None,
     if show:
         fig.show()
     return fig,axis
+
+# same as before, but places plot on an existing axis, rather than creating a new one
+def plot_xarray_to_axis(xarr, ax, vmin=None, vmax=None, cmap='viridis',
+                add_colorbar=False, cbar_kwargs={}, title="", subplot_kw={}):
+    if vmin is None:
+        vmin = np.nanmin(xarr)
+    if vmax is None:
+        vmax =np.nanmax(xarr)
+
+    fig, axis = plt.subplots(nrows=1, ncols=1, figsize=(figsize_x,figsize_y),
+                   subplot_kw=subplot_kw)
+    if add_colorbar:
+        extend = 'neither'
+        if vmin > np.nanmin(xarr.values):
+            if vmax < np.nanmax(xarr.values):
+                extend = 'both'
+            else:
+                extend = 'min'
+        else:
+            if vmax < np.nanmax(xarr.values):
+                extend = 'max'
+        xarr.plot.imshow(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar,
+                    extend=extend, cbar_kwargs=cbar_kwargs,transform = subplot_kw['projection'])
+    else:
+        xarr.plot.imshow(ax=axis, vmin=vmin, vmax=vmax, cmap=cmap, add_colorbar=add_colorbar,transform = subplot_kw['projection'])
+    axis.set_aspect("equal")
+    axis.set_title(title,fontsize=16)
+    return 0
