2c1bf7025e8b8b698fea0e09f09216c83752f751
David Milodowski <d.t.milodowski@ed.ac.uk>
2020-01-20 13:23:53 +0000

    automatically translate csv file to point shapefiles

---
+++ src/data_preparation/csv_to_shp.py
@@ -0,0 +1,45 @@
+import csv
+from shapely.geometry import Point, mapping
+from fiona import collection
+
+def is_number(s):
+    """ Returns True is string is a number. """
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
+
+csvfile = '../../data/field_calibration/Kiuic_AGB_2018_LiDAR.csv'
+outfile = csvfile[:-4]+'.shp'
+coord_x = 'x'
+coord_y = 'y'
+
+print(outfile)
+
+properties_dtype = {}
+
+with open(csvfile, 'rt') as f:
+        reader = csv.DictReader(f)
+        fields = reader.fieldnames
+        example = next(reader)
+        for field in fields:
+            if is_number(example[field]):
+                properties_dtype[field]='float'
+            else:
+                properties_dtype[field]='str'
+
+schema = { 'geometry': 'Point', 'properties': properties_dtype }
+with collection(
+    outfile, "w", "ESRI Shapefile", schema) as output:
+    with open(csvfile, 'rt') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            point = Point(float(row[coord_x]), float(row[coord_y]))
+            properties = {}
+            for field in fields:
+                properties[field]=row[field]
+            output.write({
+                'properties': properties,
+                'geometry': mapping(point)
+            })
