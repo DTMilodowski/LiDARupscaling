9782a4fbe8c27782190cf81e284ae102664cf9c9
David Milodowski <d.t.milodowski@ed.ac.uk>
2020-01-20 15:02:36 +0000

    adding bounds check to polygon sampling

--- src/lidar_calibration/geospatial_tools.py
+++ src/lidar_calibration/geospatial_tools.py
@@ -26,6 +26,9 @@ Returns:
 """
 def sample_raster_by_polygon(raster,polygon,x_dim='x',y_dim='y',label = None):
 
+    results={}
+    results['id'] = label
+
     if label is None:
         label = str('ID%i' % np.random.random()*10**9)
 
@@ -44,44 +47,49 @@ def sample_raster_by_polygon(raster,polygon,x_dim='x',y_dim='y',label = None):
     Xmin = polygon.bounds[0]; Xmax = polygon.bounds[2]
     Ymin = polygon.bounds[1]; Ymax = polygon.bounds[3]
 
-    x_mask = np.all((X>=Xmin-rad,X<=Xmax+rad),axis=0)
-    y_mask = np.all((Y>=Ymin-rad,Y<=Ymax+rad),axis=0)
-    mask = np.ix_(y_mask,x_mask)
+    if(np.all((Xmin-rad>=X.min(),Xmax+rad<=X.max(),Ymin-rad>=Y.min(),Ymax+rad<=Y.max()))):
 
-    X_sub = X[x_mask]
-    Y_sub = Y[y_mask]
-    X1 = X_sub-dX/2.
-    X2 = X_sub+dX/2.
-    Y1 = Y_sub-dY/2.
-    Y2 = Y_sub+dY/2.
+        x_mask = np.all((X>=Xmin-rad,X<=Xmax+rad),axis=0)
+        y_mask = np.all((Y>=Ymin-rad,Y<=Ymax+rad),axis=0)
+        mask = np.ix_(y_mask,x_mask)
 
-    cols_sub = x_mask.sum()
-    rows_sub = y_mask.sum()
-    raster_sub = np.zeros((rows_sub,cols_sub,bands))
-    for bb in range(0,bands):
-        raster_sub[bb,:,:] = raster[bb,:,:][mask]
+        X_sub = X[x_mask]
+        Y_sub = Y[y_mask]
+        X1 = X_sub-dX/2.
+        X2 = X_sub+dX/2.
+        Y1 = Y_sub-dY/2.
+        Y2 = Y_sub+dY/2.
 
-    # now find all pixels from subset that at least partially fall within the polygon boundary
-    in_polygon = np.zeros((rows_sub,cols_sub))
-    for rr in range(0, rows_sub):
-        for cc in range(0, cols_sub):
-            # create a pixel polygon
-            pixel = Polygon(np.asarray([(X1[cc],Y1[rr]),(X2[cc],Y1[rr]),(X2[cc],Y2[rr]),(X1[cc],Y2[rr])]))
-            # calculate the intersection fraction
-            in_polygon[rr,cc] = pixel.intersection(polygon).area/pixel.area
+        cols_sub = x_mask.sum()
+        rows_sub = y_mask.sum()
+        raster_sub = np.zeros((rows_sub,cols_sub,bands))
+        for bb in range(0,bands):
+            raster_sub[bb,:,:] = raster[bb,:,:][mask]
 
-    # calculate the weighted average
-    weighted_average = np.zeros(bands)*np.nan
-    for bb in bands:
-        weighted_average[bb] = np.nansum(raster_sub[bb]*in_plot)/np.nansum(in_neighbourhood)
+        # now find all pixels from subset that at least partially fall within the polygon boundary
+        in_polygon = np.zeros((rows_sub,cols_sub))
+        for rr in range(0, rows_sub):
+            for cc in range(0, cols_sub):
+                # create a pixel polygon
+                pixel = Polygon(np.asarray([(X1[cc],Y1[rr]),(X2[cc],Y1[rr]),(X2[cc],Y2[rr]),(X1[cc],Y2[rr])]))
+                # calculate the intersection fraction
+                in_polygon[rr,cc] = pixel.intersection(polygon).area/pixel.area
 
-    results={}
-    results['id'] = label
-    results['weights']=in_plot.copy()
-    results['raster_values']=raster_sub.copy()
-    results['x']=X_sub.copy()
-    results['y']=Y_sub.copy()
-    results['weighted_average'] = weighted_average
+        # calculate the weighted average
+        weighted_average = np.zeros(bands)*np.nan
+        for bb in bands:
+            weighted_average[bb] = np.nansum(raster_sub[bb]*in_plot)/np.nansum(in_neighbourhood)
+
+        results={}
+        results['id'] = label
+        results['weights']=in_plot.copy()
+        results['raster_values']=raster_sub.copy()
+        results['x']=X_sub.copy()
+        results['y']=Y_sub.copy()
+        results['weighted_average'] = weighted_average
+        results['status'] = 'PASS'
+    else:
+        results['status'] = 'FAIL'
 
     return results
 
@@ -114,6 +122,8 @@ def sample_raster_by_point_neighbourhood(raster,point,radius,x_dim='x',y_dim='y'
     Y = raster[y_dim].values
     dX = X[1]-X[0]
     dY = Y[1]-Y[0]
+    rad = np.sqrt(2.*max((dX/2.)**2,(dY/2.)**2))
+
     if len(raster.values.shape)==2:
         bands = 1
         raster=raster.expand_dims('band')
@@ -124,7 +134,6 @@ def sample_raster_by_point_neighbourhood(raster,point,radius,x_dim='x',y_dim='y'
     Xmin = neighbourhood.bounds[0]; Xmax = neighbourhood.bounds[2]
     Ymin = neighbourhood.bounds[1]; Ymax = neighbourhood.bounds[3]
 
-    rad = np.sqrt(2.*max((dX/2.)**2,(dY/2.)**2))
     if(np.all((Xmin-rad>=X.min(),Xmax+rad<=X.max(),Ymin-rad>=Y.min(),Ymax+rad<=Y.max()))):
 
         x_mask = np.all((X>=Xmin-rad,X<=Xmax+rad),axis=0)
