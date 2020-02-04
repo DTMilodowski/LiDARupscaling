71374dd882f85d2b3aa0af83c459981d9a62b79e
David Milodowski <d.t.milodowski@ed.ac.uk>
2020-02-04 12:08:26 +0000

    refining polygon intersection script to effectively mirror the point neighbourhood function

--- src/lidar_calibration/geospatial_tools.py
+++ src/lidar_calibration/geospatial_tools.py
@@ -26,72 +26,63 @@ Returns:
 """
 def sample_raster_by_polygon(raster,polygon,x_dim='x',y_dim='y',label = None):
 
-    results={}
-    results['id'] = label
-
+    # generate 'unique' ID label if none specified
     if label is None:
         label = str('ID%i' % np.random.random()*10**9)
 
-    X = raster[x_dim].values
-    Y = raster[y_dim].values
-    dX = X[1]-X[0]
-    dY = Y[1]-Y[0]
+    results={}
+    results['id'] = label
+
+    # Get dimension info etc
+    X = raster[x_dim].values; Y = raster[y_dim].values
+    dX = X[1]-X[0]; dY = Y[1]-Y[0]
     rad = np.sqrt(2.*max((dX/2.)**2,(dY/2.)**2))
 
+    # Check number of bands is consistent with expectations for subsequent processing
     if len(raster.values.shape)==2:
         bands = 1
+        raster=raster.expand_dims('band')
     elif len(raster.values.shape)==3:
         bands = raster.shape[0]
 
     # Generate mask around AOI to make subsequent code more efficient
-    Xmin = polygon.bounds[0]; Xmax = polygon.bounds[2]
-    Ymin = polygon.bounds[1]; Ymax = polygon.bounds[3]
-
+    Xmin,Ymin,Xmax,Ymax = polygon.bounds
     if(np.all((Xmin-rad>=X.min(),Xmax+rad<=X.max(),Ymin-rad>=Y.min(),Ymax+rad<=Y.max()))):
 
-        x_mask = np.all((X>=Xmin-rad,X<=Xmax+rad),axis=0)
-        y_mask = np.all((Y>=Ymin-rad,Y<=Ymax+rad),axis=0)
-        mask = np.ix_(y_mask,x_mask)
+        if dY<0:
+            raster_sub = raster.sel(x=slice(Xmin-rad,Xmax+rad),y=slice(Ymax+rad,Ymin-rad))
+        else:
+            raster_sub = raster.sel(x=slice(Xmin-rad,Xmax+rad),y=slice(Ymin-rad,Ymax+rad))
 
-        X_sub = X[x_mask]
-        Y_sub = Y[y_mask]
-        X1 = X_sub-dX/2.
-        X2 = X_sub+dX/2.
-        Y1 = Y_sub-dY/2.
-        Y2 = Y_sub+dY/2.
-
-        cols_sub = x_mask.sum()
-        rows_sub = y_mask.sum()
-        raster_sub = np.zeros((bands,rows_sub,cols_sub))
-        for bb in range(0,bands):
-            raster_sub[bb,:,:] = raster[bb,:,:][mask]
+        X1 = raster_sub[x_dim].values-dX/2.; X2 = raster_sub[x_dim].values+dX/2.
+        Y1 = raster_sub[y_dim].values-dY/2.; Y2 = raster_sub[y_dim].values+dY/2.
 
         # now find all pixels from subset that at least partially fall within the polygon boundary
-        in_polygon = np.zeros((rows_sub,cols_sub))
-        for rr in range(0, rows_sub):
-            for cc in range(0, cols_sub):
+        #in_neighbourhood = np.zeros((rows_sub,cols_sub))
+        in_neighbourhood = raster_sub[0].copy(deep=True)
+        in_neighbourhood.values=in_neighbourhood.values.astype('float')
+        in_neighbourhood.values*=np.nan
+        for rr in range(0,raster_sub[y_dim].values.size):
+            for cc in range(0,raster_sub[x_dim].values.size):
                 # create a pixel polygon
                 pixel = Polygon(np.asarray([(X1[cc],Y1[rr]),(X2[cc],Y1[rr]),(X2[cc],Y2[rr]),(X1[cc],Y2[rr])]))
                 # calculate the intersection fraction
-                in_polygon[rr,cc] = pixel.intersection(polygon).area/pixel.area
+                in_neighbourhood.values[rr,cc] = pixel.intersection(polygon).area/pixel.area
 
         # calculate the weighted average
         weighted_average = np.zeros(bands)*np.nan
         for bb in range(0,bands):
-            weighted_average[bb] = np.nansum(raster_sub[bb]*in_plot)/np.nansum(in_neighbourhood)
+            weighted_average[bb] = np.nansum(raster_sub.values[bb]*in_neighbourhood.values)/np.nansum(in_neighbourhood.values)
 
-        results={}
-        results['id'] = label
-        results['weights']=in_plot.copy()
-        results['raster_values']=raster_sub.copy()
-        results['x']=X_sub.copy()
-        results['y']=Y_sub.copy()
-        results['weighted_average'] = weighted_average
+        # Store results for this sample
+        results['weights']=in_neighbourhood.copy(deep=True)
+        results['raster_values']=raster_sub.copy(deep=True)
+        results['weighted_average'] = weighted_average.copy()
         results['status'] = 'PASS'
     else:
         results['status'] = 'FAIL'
-
     return results
+
 
 """
 sample_raster_by_point_neighbourhood
@@ -151,13 +142,11 @@ def sample_raster_by_point_neighbourhood(raster,point,radius,x_dim='x',y_dim='y'
         mask_inner = np.ix_(y_mask,x_mask)
 
         # now find all pixels from subset that at least partially fall within the polygon boundary
-        #in_neighbourhood = np.zeros((rows_sub,cols_sub))
         in_neighbourhood = raster_sub[0].copy(deep=True)
         in_neighbourhood.values=in_neighbourhood.values.astype('float')
         in_neighbourhood.values*=np.nan
         in_neighbourhood.values[mask_inner]=1
-        #for rr in range(0, rows_sub):
-        #    for cc in range(0, cols_sub):
+
         for rr in range(0,raster_sub[y_dim].values.size):
             for cc in range(0,raster_sub[x_dim].values.size):
                 if np.isnan(in_neighbourhood.values[rr,cc]):
