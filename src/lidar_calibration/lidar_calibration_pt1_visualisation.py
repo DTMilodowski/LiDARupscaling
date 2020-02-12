3751eeda1007316292801902e161d340ecf8f89d
David Milodowski <d.t.milodowski@ed.ac.uk>
2020-02-12 13:27:39 +0000

    positional uncertainty modelled

--- src/lidar_calibration/lidar_calibration_pt1_visualisation.py
+++ src/lidar_calibration/lidar_calibration_pt1_visualisation.py
@@ -128,10 +128,6 @@ for pp, plot in enumerate(plot_clusters):
 dem.close(); dem=None
 gc.collect()
 
-# save for future use
-collated_results = {'chm':chm_results,'dem':dem_results,'inventory':inventory_AGB}
-np.savez(outfile,collated_results)
-
 # CALIBRATION STATISTICS
 ID=[]; AGB = []; TCH = []; COVER = []; NODATA = []
 for plot in chm_results.keys():
@@ -149,7 +145,71 @@ for plot in chm_results.keys():
 # Convert to np arrays for conditional indexing convenience
 AGB = np.asarray(AGB); TCH = np.asarray(TCH); COVER = np.asarray(COVER); ID = np.asarray(ID)
 
-# Plot the figure
+"""
+Explore positional uncertainty and it's impact on TCH estimation
+"""
+chm_results_mc = {}
+positional_error = 5.
+n_iter = 100
+# SAMPLE RASTER FOR EACH PLOT NEIGHBOURHOOD
+plot_clusters = plot_info.keys()
+for pp, plot in enumerate(plot_clusters):
+    # Generate mask around AOI to make subsequent code more efficient
+    Xmin,Ymin,Xmax,Ymax = plot_info[plot]['buffer'].bounds
+    Xmin-=50;Xmax+=50;Ymin-=50;Ymax+=50
+    if dY<0:
+        chm_sub = chm.sel(x=slice(Xmin,Xmax),y=slice(Ymax,Ymin))
+    else:
+        chm_sub = chm.sel(x=slice(Xmin,Xmax),y=slice(Ymin,Ymax))
+
+    for subplot in inventory:
+        if ('%.0f' % subplot['properties']['plot'])==plot:
+            id = '%.0f.%.0f' % (subplot['properties']['plot'],subplot['properties']['subplot'])
+            print('\t\tprocessing %s (cluster %i/%i)' % (id,pp+1,len(plot_clusters)),end='\r')
+            if chm_sub.values.size>0:
+                tch_mc = np.zeros(n_iter)*np.nan
+                for ii in range(0,n_iter):
+                    xerr = np.random.randn()*positional_error
+                    yerr = np.random.randn()*positional_error
+                    plot_centre = Point(subplot['geometry']['coordinates'][0]+xerr,subplot['geometry']['coordinates'][1]+yerr)
+                    results_iter = gst.sample_raster_by_point_neighbourhood(chm_sub,plot_centre,radius,x_dim='x',y_dim='y',label = id)
+                    if results_iter['status']=='PASS':
+                        tch_mc[ii] = results_iter['weighted_average'][0]
+
+                    chm_results[id]['tch_mc_position']=tch_mc.copy()
+
+                chm_results_mc[id] = {'mean':np.mean(tch_mc),'sd':np.std(tch_mc),
+                                        'med':np.median(tch_mc),'2.5perc':np.percentile(tch_mc,2.5),
+                                        '97.5perc':np.percentile(tch_mc,97.5),
+                                        '25perc':np.percentile(tch_mc,25),
+                                        '50perc':np.percentile(tch_mc,50),
+                                        '75perc':np.percentile(tch_mc,75),
+                                        '95perc':np.percentile(tch_mc,95)}
+
+# CALIBRATION STATISTICS
+mean=[]; sd = []; CI95_l = []; CI95_u = []; CI50_l = []; CI50_u = []
+for plot in chm_results.keys():
+    id = chm_results[plot]['id']
+    if chm_results[id]['status']=='PASS':
+        if chm_results[id]['weighted_average']>0:
+            if np.sum(np.isnan(chm_results[id]['raster_values'][0])*chm_results[id]['weights'])<0.05*np.sum(chm_results[id]['weights']):
+                mean.append(chm_results_mc[id]['mean'])
+                sd.append(chm_results_mc[id]['sd'])
+                CI95_l.append(chm_results_mc[id]['2.5perc'])
+                CI95_u.append(chm_results_mc[id]['97.5perc'])
+                CI50_l.append(chm_results_mc[id]['25perc'])
+                CI50_u.append(chm_results_mc[id]['75perc'])
+SD=np.array(sd);MEAN=np.array(mean);CI95_l=np.array(CI95_l);CI95_u=np.array(CI95_u)
+CI50_l=np.array(CI50_l);CI50_u=np.array(CI50_u)
+
+# save for future use
+collated_results = {'chm':chm_results,'dem':dem_results,'inventory':inventory_AGB}
+np.savez(outfile,collated_results)
+
+"""
+PLOTTING
+"""
+# Plot the figure (original)
 fig,fig_axes = plt.subplots(nrows=2,ncols=3,figsize=[12,7])
 axes=fig_axes.flatten()
 axes[0].plot(TCH,AGB,'.',color='black')
@@ -172,4 +232,35 @@ for ii,plot_id in enumerate(plots_to_plot):
     axes[ii+1].set_ylabel('')
 fig.tight_layout()
 fig.savefig('%slidar_TCH_vs_field_AGB_400m.png' % path2fig)
+fig.show()
+
+# Plot the figure with montecarlo postion errors
+fig,fig_axes = plt.subplots(nrows=2,ncols=3,figsize=[12,7])
+axes=fig_axes.flatten()
+axes[0].plot(MEAN,AGB,'.',color='black')
+#axes[0].errorbar(MEAN,AGB,xerr=SD,
+#                    marker='',linestyle='',color='0.5',linewidth=0.5)
+axes[0].errorbar(MEAN,AGB,xerr=(MEAN-CI95_l,CI95_u-MEAN),
+                    marker='',linestyle='',color='0.67',linewidth=0.5)
+axes[0].errorbar(MEAN,AGB,xerr=(MEAN-CI50_l,CI50_u-MEAN),
+                    marker='',linestyle='',color='0.33',linewidth=0.75)
+axes[0].set_title('TCH vs. inventory AGB')
+axes[0].set_xlabel('mean TCH / m')
+axes[0].set_ylabel('field AGB / Mg ha$^{-1}$')
+for ii,plot_id in enumerate(plots_to_plot):
+    plot_boundary = mpatches.Circle((0,0),radius,ec='white',fill=False)#,fc=None)
+    #im = axes[ii+1].imshow(chm_results[plot_id]['chm_raster'][0],vmin=0,vmax=23)
+    chm_results[plot_id]['chm_raster'].plot(ax=axes[ii+1], vmin=0, vmax=21,
+                extend='max', cbar_kwargs={'label':'height / m'})
+    axes[ii+1].add_artist(plot_boundary)
+    axes[ii+1].set_title('plot %s; AGB = %.1f Mg/ha' % (plot_id,AGB[ID==plot_id]))
+    axes[0].plot(MEAN[ID==plot_id],AGB[ID==plot_id],'.',color='#2db27d')#'#bc1655')
+    axes[0].annotate(' %s' % plot_id,xy=(TCH[ID==plot_id],AGB[ID==plot_id]),color='#2db27d')
+    if ii>=2:
+        axes[ii+1].set_xlabel('distance / m')
+    else:
+        axes[ii+1].set_xlabel('')
+    axes[ii+1].set_ylabel('')
+fig.tight_layout()
+fig.savefig('%slidar_TCH_vs_field_AGB_400m_after_mc.png' % path2fig)
 fig.show()
