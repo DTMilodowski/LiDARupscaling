695e08c0bf15d7a46069cfba229ac82f12c7bf92
David Milodowski <d.t.milodowski@ed.ac.uk>
2020-11-06 09:54:51 +0000

    qq-plot and moving average filter

--- src/lidar_calibration/stats_tools.py
+++ src/lidar_calibration/stats_tools.py
@@ -5,6 +5,12 @@ useful stats functions
 D.T.Milodowski
 """
 import numpy as np
+from scipy import stats
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
 # Calculate correction factor due to bias in mean fitted in log-transformed
 # parameter space (Baskerville 1972)
 def calculate_baskervilleCF(log_y,log_yhat):
@@ -33,3 +39,61 @@ def weighted_quantiles(data,weights,quantiles=[.01,.025,.25,.5,.75,.975,.99]):
     quantile_vals = np.interp(quantiles,weighted_quantiles,data_sub)
 
     return quantile_vals
+
+# Generate a qq plot woth confidence interval for normal distribution based on
+# sample size (default is 95%)
+def qq_plot(variable,fig=None,ax=None,ci=.95,ylabel = "",show=True,savename = None):
+
+    N = variable.size
+    v = np.zeros(N,dtype=np.float64)
+    v[-1] = 0.5**(1./N)
+    v[0] = 1 - v[-1]
+    i = np.arange(2,N)
+    v[1:-1]=(i-0.3175)/(N+0.365)
+    x_qq = stats.norm.ppf(v)
+
+    ci_low_quantile = (1. - ci)/2.
+    ci_upp_quantile = 1 - ci_low_quantile
+
+    if ax is None:
+        fig,ax=plt.subplots(nrows=1,ncols=1)
+
+    ci_band = np.zeros((1000,N))
+
+    rmse=np.sqrt(np.mean(variable**2))
+    for jj in range(1000):
+        ci_band[jj]=np.random.normal(scale=rmse,size=N)
+        ci_band[jj].sort()
+
+    ci_upp = np.percentile(ci_band,ci_upp_quantile*100,axis=0)
+    ci_low = np.percentile(ci_band,ci_low_quantile*100,axis=0)
+    ax.fill_between(x_qq,ci_low,ci_upp,color='0.5',alpha=0.5)
+
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel('standard normal variate')
+
+    sm.qqplot(variable, marker='.',ax=ax,line='s')
+
+    if show:
+        fig.show()
+
+    if savename is not None:
+        fig.savefig(savename)
+
+# moving average filter
+def moving_bin_residual(x_mod,x_obs,bin_halfwidth=10,post_spacing=1):
+    res = x_mod-x_obs
+    x = np.arange(x_mod.min(),x_mod.max(),post_spacing)
+    res_ = np.zeros(x.shape)*np.nan
+    res_sd = np.zeros(x.shape)*np.nan
+    for ii,xi in enumerate(x):
+        if xi-x.min() < bin_halfwidth + 1:
+            mask = x_mod<(x.min()+2*bin_halfwidth+1)
+        elif x.max()-xi < bin_halfwidth + 1:
+            mask = x_mod>(x.max()-2*bin_halfwidth+1)
+        else:
+            mask = (x_mod>=xi-bin_halfwidth) * (x_mod<xi+bin_halfwidth)
+
+        res_[ii] = np.mean(res[mask])
+        res_sd[ii] = np.std(res[mask])
+    return x,res_,res_sd
